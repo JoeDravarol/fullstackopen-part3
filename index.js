d2b901609e331128dbbec08446ea2afc81a58528
Joe <joedravarol@gmail.com>
2019-10-28 22:32:08 +0000

    Complete phonebook (backend) step 3

--- index.js
+++ index.js
@@ -36,6 +36,17 @@ app.get('/info', (request, response) => {
     `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const port = 3001
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
