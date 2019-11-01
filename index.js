c851a0aaed7d3b230266c295a5217619fedc6d01
Joe <joedravarol@gmail.com>
2019-11-01 14:09:35 +0000

    Add validation to schema and appropriate error handler to post request

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name && !body.number) {
@@ -85,9 +85,10 @@ app.post('/api/persons', (request, response) => {
     .then(p => {
       response.json(p.toJSON())
     })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
   const person = {
@@ -107,6 +108,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
